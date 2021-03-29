68c124f0bae8f048f7ce0cd551d82eec586ba492
Muhammad Naqib <nakibhassan74@gmail.com>
2021-03-29 14:23:49 +0600

    fix problem

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(cors());
 const port = 5000
 
 
+app.get('/',(req,res) =>{
+  res.send('hello from db its working')
+})
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gaubw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
